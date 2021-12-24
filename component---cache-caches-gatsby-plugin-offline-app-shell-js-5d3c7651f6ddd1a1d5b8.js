cb2a8b6343d34452ec484968bc071d7ee086aed1
Tony Vo <ttrung.vo@gmail.com>
2021-12-23 21:40:30 -0700

    Updates

--- component---cache-caches-gatsby-plugin-offline-app-shell-js-5d3c7651f6ddd1a1d5b8.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_bootstrap=self.webpackChunkgatsby_starter_bootstrap||[]).push([[306],{1930:function(t,e,r){"use strict";var a=r(5318);e.__esModule=!0,e.default=void 0;var n=a(r(5354)),u=a(r(7294)),o=function(t){function e(){return t.apply(this,arguments)||this}return(0,n.default)(e,t),e.prototype.render=function(){return u.default.createElement(u.default.Fragment,null)},e}(u.default.Component);e.default=o}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-5d3c7651f6ddd1a1d5b8.js.map
---
+++ component---src-pages-404-js-df6c8d83b9caeebac892.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktvo_blog_generator=self.webpackChunktvo_blog_generator||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),o=a(7198),l=a(3751);e.default=function(t){var e=t.data,a=t.location,r=e.site.siteMetadata.title;return n.createElement(o.Z,{location:a,title:r},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-df6c8d83b9caeebac892.js.map
